a0b62b5f64290b9a30d962d6b39160122a9ff059
Everything-Thats-Radical <96023754+Everything-Thats-Radical@users.noreply.github.com>
2025-11-25 16:49:56 -0600

    Feeder Time change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoalAUTO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoalAUTO.java
@@ -66,7 +66,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class GoalAUTO extends OpMode
 {
 
-    final double FEED_TIME = 0.20; //The feeder servos run this long when a shot is requested.
+    final double FEED_TIME = 0.60; //The feeder servos run this long when a shot is requested.
 
     /*
      * When we control our launcher motor, we are using encoders. These allow the control system
@@ -74,8 +74,8 @@ public class GoalAUTO extends OpMode
      * velocity. Here we are setting the target and minimum velocity that the launcher should run
      * at. The minimum velocity is a threshold for determining when to fire.
      */
-    final double LAUNCHER_TARGET_VELOCITY = 1300;
-    final double LAUNCHER_MIN_VELOCITY = 1100;
+    final double LAUNCHER_TARGET_VELOCITY = 1250;
+    final double LAUNCHER_MIN_VELOCITY = 1050;
 
     /*
      * The number of seconds that we wait between each of our 3 shots from the launcher. This
